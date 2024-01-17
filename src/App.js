ace48e0d8293483eb30f4244e908b502ac886f20
Asia Valentine <asiavalentine22@gmail.com>
2024-01-17 15:05:41 -0500

    edits

--- src/App.js
+++ src/App.js
@@ -34,17 +34,7 @@ function App() {
     const [fullInput, setFullInput] = useState('')
     const [isSignedIn, setIsSignedIn] = useState(true)
 
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    const firebaseConfig = {
-        apiKey: "AIzaSyDfYvcVytioWmYX0sG-_IlA4kWHx40RzHg",
-        authDomain: "clone-29217.firebaseapp.com",
-        databaseURL: "https://clone-29217.firebaseio.com",
-        projectId: "clone-29217",
-        storageBucket: "clone-29217.appspot.com",
-        messagingSenderId: "172397596944",
-        appId: "1:172397596944:web:6f31c3b472a6e0ed5a1595",
-        //measurementId: "G-VD31RC3610"
-    };
+  
 
     if(!firebase.apps.length){
         firebase.initializeApp(firebaseConfig);
